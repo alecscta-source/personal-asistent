f9a8ca1ad23b2651e2b0a9a81dc549520803b22f
alecscta-source <alecscta@gmail.com>
2026-09-03 16:29:06 +0300

    Load custom team portraits

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles.css'
 import './extra.css'
 import './visual-v2.css'
+import './team-v2.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
