20ec7f5a71e83f65c32b1b8dc10714805fe51dcd
alecscta-source <alecscta@gmail.com>
2026-09-05 11:11:53 +0300

    Mount Bianca command bar on office home

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import './voice-guard'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import HomeCommand from './HomeCommand'
 import './styles.css'
 import './extra.css'
 import './visual-v2.css'
@@ -9,9 +10,15 @@ import './team-v2.css'
 import './pages-v2.css'
 import './office-final.css'
 import './voice-mode.css'
+import './home-command.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+const commandRoot=document.createElement('div')
+commandRoot.id='bianca-home-command-root'
+document.body.appendChild(commandRoot)
+ReactDOM.createRoot(commandRoot).render(<HomeCommand />)
