da5094d6f6811886cd89eae14d27e37e18fa8c36
alecscta-source <alecscta@gmail.com>
2026-09-08 14:07:42 +0300

    Restore original office calendar image

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import HomeCommand from './HomeCommand'
-import DeskCalendar from './DeskCalendar'
 import './styles.css'
 import './extra.css'
 import './visual-v2.css'
@@ -13,12 +12,10 @@ import './pages-v2.css'
 import './office-final.css'
 import './voice-mode.css'
 import './home-command.css'
-import './desk-calendar.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
     <HomeCommand />
-    <DeskCalendar />
   </React.StrictMode>,
 )
